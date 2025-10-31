230f578055cf2528eefafa7b238f5cab45d9e9ab
Igor Guliaev <igor.s.gulyaev@gmail.com>
2025-10-31 20:04:45 +0000

    Add update password page

---
+++ src/app/update-password/page.tsx
@@ -0,0 +1,24 @@
+import { AuthPage } from "@components/auth-page"
+import { authProviderServer } from "@providers/auth-provider/auth-provider.server"
+import { redirect } from "next/navigation"
+
+export default async function UpdatePassword() {
+  const data = await getData()
+
+  // User must be authenticated to update password
+  if (!data.authenticated) {
+    redirect('/login')
+  }
+
+  return <AuthPage type="updatePassword" />
+}
+
+async function getData() {
+  const { authenticated, redirectTo, error } = await authProviderServer.check()
+
+  return {
+    authenticated,
+    redirectTo,
+    error,
+  }
+}
