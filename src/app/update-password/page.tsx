4562ec782eb71fc39edca1354ead1741fa75c787
Igor Guliaev <igor.s.gulyaev@gmail.com>
2025-10-31 20:10:46 +0000

    Fix review findings

--- src/app/update-password/page.tsx
+++ src/app/update-password/page.tsx
@@ -1,24 +1,24 @@
-import { AuthPage } from "@components/auth-page"
-import { authProviderServer } from "@providers/auth-provider/auth-provider.server"
-import { redirect } from "next/navigation"
+import { AuthPage } from "@components/auth-page";
+import { authProviderServer } from "@providers/auth-provider/auth-provider.server";
+import { redirect } from "next/navigation";
 
 export default async function UpdatePassword() {
-  const data = await getData()
+  const data = await getData();
 
   // User must be authenticated to update password
   if (!data.authenticated) {
-    redirect('/login')
+    redirect('/login');
   }
 
-  return <AuthPage type="updatePassword" />
+  return <AuthPage type="updatePassword" />;
 }
 
 async function getData() {
-  const { authenticated, redirectTo, error } = await authProviderServer.check()
+  const { authenticated, redirectTo, error } = await authProviderServer.check();
 
   return {
     authenticated,
     redirectTo,
     error,
-  }
+  };
 }
